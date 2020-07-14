ab6352838c4b444e3048759d7502cc6fa8c0786b
Ronald Libago <me@ronaldlibago.dev>
2020-07-13 19:10:40 -0700

    Add simple submit to GameScreen

--- src/pages/game-screen.tsx
+++ src/pages/game-screen.tsx
@@ -4,11 +4,19 @@ import { Slider } from '../components/slider';
 export const GameScreen = () => {
   const [sliderValue, setSliderValue] = React.useState(100);
 
+  const submitSliderInput = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(sliderValue);
+  };
+
   return (
     <>
       <h1>Frequency</h1>
-      <Slider initialValue={sliderValue} onChange={setSliderValue} />
-      <p>{sliderValue}</p>
+      <form onSubmit={submitSliderInput}>
+        <Slider initialValue={sliderValue} onChange={setSliderValue} />
+        <p>{sliderValue}</p>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
