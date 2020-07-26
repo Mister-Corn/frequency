f31f80aa98f78cb6622cc5d114f604e72aedeed8
Ronald Libago <me@ronaldlibago.dev>
2020-07-26 08:54:35 -0700

    Connect GameScreen to context

--- src/pages/game-screen.tsx
+++ src/pages/game-screen.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
 import { Slider } from '../components/slider';
+import { GameContext } from '../scripts/context-provider';
+import { GameActions } from '../scripts/state-reducer';
 
 export const GameScreen = () => {
-  const [sliderValue, setSliderValue] = React.useState(100);
+  const {
+    state: {
+      game: { target },
+    },
+    dispatch,
+  } = React.useContext(GameContext);
+  const [sliderValue, setSliderValue] = React.useState(target);
 
   const submitSliderInput = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(sliderValue);
   };
 
+  React.useEffect(() => {
+    dispatch({ type: GameActions.START_GAME });
+  }, []);
+
+  React.useEffect(() => {
+    setSliderValue(target);
+  }, [target]);
+
   return (
     <>
       <h1>Frequency</h1>
       <form onSubmit={submitSliderInput}>
-        <Slider initialValue={sliderValue} onChange={setSliderValue} />
+        <Slider value={sliderValue} onChange={setSliderValue} />
         <p>{sliderValue}</p>
         <button type="submit">Submit</button>
       </form>
